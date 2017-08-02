030948ca6e65509e747c7f788101c493d141f461
nathantspencer <nathantspencer@gmail.com>
2017-08-02 15:33:32 -0400

    removed jquery

--- distract-overflow.js
+++ distract-overflow.js
@@ -1,29 +1,25 @@
-$( document ).ready(function() {
+var hotNetworkQuestions = document.getElementById("hot-network-questions");
+var hireme = document.getElementById("hireme");
+var communityBulletin = document.getElementsByClassName("community-bulletin");
+var newsletterAd = document.getElementById("newsletter-ad");
+var chatFeature = document.getElementById("chat-feature");
 
-	var hotNetworkQuestions = document.getElementById("hot-network-questions");
-	var hireme = document.getElementById("hireme");
-	var communityBulletin = document.getElementsByClassName("community-bulletin");
-	var newsletterAd = document.getElementById("newsletter-ad");
-	var chatFeature = document.getElementById("chat-feature");
-	
-	if(hotNetworkQuestions) {
-		hotNetworkQuestions.style.display = "none";
-	}
-	
-	if(hireme) {
-		hireme.style.display = "none";
-	}
-	
-	if(communityBulletin.length > 0) {
-		communityBulletin[0].style.display = "none";
-	}
-	
-	if(newsletterAd) {
-		newsletterAd.style.display = "none";
-	}
-	
-	if(chatFeature) {
-		chatFeature.style.display = "none";
-	}
-	
-})
+if(hotNetworkQuestions) {
+	hotNetworkQuestions.style.display = "none";
+}
+
+if(hireme) {
+	hireme.style.display = "none";
+}
+
+if(communityBulletin.length > 0) {
+	communityBulletin[0].style.display = "none";
+}
+
+if(newsletterAd) {
+	newsletterAd.style.display = "none";
+}
+
+if(chatFeature) {
+	chatFeature.style.display = "none";
+}
