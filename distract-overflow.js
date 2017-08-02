3091b977bd1864bfb9a8b24321a7fecad86c1cc9
nathantspencer <nathantspencer@gmail.com>
2017-08-02 13:55:45 -0400

    added jquery, hides a couple more elements

--- distract-overflow.js
+++ distract-overflow.js
@@ -1,3 +1,16 @@
-window.onload = function(){ 
-	document.getElementById("hot-network-questions").style.visibility = "hidden";
-}
+$( document ).ready(function() {
+
+	var hotNetworkQuestions = document.getElementById("hot-network-questions");
+	var hireme = document.getElementById("hireme");
+	
+	if(hotNetworkQuestions)
+	{
+		hotNetworkQuestions.style.display = "none";
+	}
+	
+	if(hireme)
+	{
+		hireme.style.display = "none";
+	}
+	
+})
