876f00d01bf4c057adfc4dc0b2cd9f5f84aa3870
nathantspencer <nathantspencer@gmail.com>
2017-08-02 14:38:35 -0400

    brace style

--- distract-overflow.js
+++ distract-overflow.js
@@ -6,28 +6,23 @@ $( document ).ready(function() {
 	var newsletterAd = document.getElementById("newsletter-ad");
 	var chatFeature = document.getElementById("chat-feature");
 	
-	if(hotNetworkQuestions)
-	{
+	if(hotNetworkQuestions) {
 		hotNetworkQuestions.style.display = "none";
 	}
 	
-	if(hireme)
-	{
+	if(hireme) {
 		hireme.style.display = "none";
 	}
 	
-	if(communityBulletin.length > 0)
-	{
+	if(communityBulletin.length > 0) {
 		communityBulletin[0].style.display = "none";
 	}
 	
-	if(newsletterAd)
-	{
+	if(newsletterAd) {
 		newsletterAd.style.display = "none";
 	}
 	
-	if(chatFeature)
-	{
+	if(chatFeature) {
 		chatFeature.style.display = "none";
 	}
 	
