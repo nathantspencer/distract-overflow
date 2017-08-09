ac0fbde7cae504a6940898ca9dafd4a0510cbe8f
Nathan Spencer <nathantspencer@gmail.com>
2017-08-09 13:42:22 -0400

    '' -> ""

--- app/background.js
+++ app/background.js
@@ -2,28 +2,28 @@ var extensionIsActive = true;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   if(extensionIsActive){
-    chrome.browserAction.setIcon({path: 'images/icon48-off.png'});
-    chrome.tabs.executeScript({ file: 'app/transition-off.js' });
+    chrome.browserAction.setIcon({ path: "images/icon48-off.png" });
+    chrome.tabs.executeScript({ file: "app/transition-off.js" });
     extensionIsActive = false;
   } else {
-    chrome.browserAction.setIcon({path: 'images/icon48.png'});
-    chrome.tabs.executeScript({ file: 'app/transition-on.js' });
+    chrome.browserAction.setIcon({ path: "images/icon48.png" });
+    chrome.tabs.executeScript({ file: "app/transition-on.js" });
     extensionIsActive = true;
   }
 });
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && !extensionIsActive) { 
-    chrome.tabs.executeScript(null, {file: 'app/transition-off.js'});
+  if (changeInfo.status === "complete" && !extensionIsActive) { 
+    chrome.tabs.executeScript(null, { file: "app/transition-off.js" });
   } else if (changeInfo.status === 'complete' && extensionIsActive) {
-    chrome.tabs.executeScript(null, {file: 'app/transition-on.js'});
+    chrome.tabs.executeScript(null, { file: "app/transition-on.js" });
   }
 });
 
 chrome.tabs.onActivated.addListener( function (tabId, changeInfo, tab) {
   if (!extensionIsActive) { 
-    chrome.tabs.executeScript(null, {file: 'app/transition-off.js'});
+    chrome.tabs.executeScript(null, { file: "app/transition-off.js" });
   } else {
-    chrome.tabs.executeScript(null, {file: 'app/transition-on.js'});
+    chrome.tabs.executeScript(null, { file: "app/transition-on.js" });
   }
 });
